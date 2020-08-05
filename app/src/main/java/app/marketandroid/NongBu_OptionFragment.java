fed39570a6b2b4274a24b5b61122553611892326
LeeJaeYoung <tekken5953@naver.com>
2020-08-05 11:21:21 +0900

    WIP

--- app/src/main/java/app/marketandroid/NongBu_OptionFragment.java
+++ app/src/main/java/app/marketandroid/NongBu_OptionFragment.java
@@ -22,18 +22,19 @@ public class NongBu_OptionFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        listView = getActivity().findViewById(R.id.option_listView);
-        for (int i = 1; i <= 10; i++){
-            list.add(i+"번 옵션");
-        }
-        adapter = new ArrayAdapter(getContext(),android.R.layout.simple_list_item_1,list);
-        listView.setAdapter(adapter);
+
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         viewGroup = (ViewGroup) inflater.inflate(R.layout.nongbu_option_fragment, container, false);
+        listView = getActivity().findViewById(R.id.option_listView);
+        for (int i = 1; i <= 10; i++){
+            list.add(i+"번 옵션");
+        }
+        adapter = new ArrayAdapter(getContext(),android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(adapter);
         return viewGroup;
     }
 }
