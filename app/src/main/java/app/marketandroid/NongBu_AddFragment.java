5ee155b18eba58b75c4d638a6ed19d344b5dc51b
LeeJaeYoung <tekken5953@naver.com>
2020-08-05 11:15:43 +0900

    WIP

--- app/src/main/java/app/marketandroid/NongBu_AddFragment.java
+++ app/src/main/java/app/marketandroid/NongBu_AddFragment.java
@@ -4,11 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -17,10 +20,27 @@ import java.util.ArrayList;
 public class NongBu_AddFragment extends Fragment {
 
     ViewGroup viewGroup;
+    Button btn1;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        btn1 = getActivity().findViewById(R.id.button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("물품 등록");
+                builder.setMessage("1번 물품 등록 다이얼");
+                builder.setPositiveButton("등록", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.show();
+            }
+        });
 
     }
 
