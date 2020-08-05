49dccb7be9adc1252143b42eda4788e9065eefd9
LeeJaeYoung <tekken5953@naver.com>
2020-08-05 11:26:48 +0900

    WIP

--- app/src/main/java/app/marketandroid/NongBu_ListFragment.java
+++ app/src/main/java/app/marketandroid/NongBu_ListFragment.java
@@ -23,19 +23,19 @@ public class NongBu_ListFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        viewGroup = (ViewGroup) inflater.inflate(R.layout.nongbu_list_fragment, container, false);
         listView = getActivity().findViewById(R.id.list_listView);
         for (int i = 1; i <= 20; i++){
             list.add(i+"번 등록 물품");
         }
         adapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1,list);
         listView.setAdapter(adapter);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        viewGroup = (ViewGroup) inflater.inflate(R.layout.nongbu_list_fragment, container, false);
+
         return viewGroup;
     }
 }
