d345ae30d2d72003eac6e7af374cf34816005e86
LeeJaeYoung <tekken5953@naver.com>
2020-08-13 10:19:27 +0900

    WIP

--- app/src/main/java/app/marketandroid/LoginActivity.java
+++ app/src/main/java/app/marketandroid/LoginActivity.java
@@ -154,7 +154,7 @@ public class LoginActivity extends AppCompatActivity {
                             Toast.makeText(LoginActivity.this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
                             keyboardUp(pwd);
                         } else {
-                            Toast.makeText(LoginActivity.this, "\t\t\t\t가입이 완료되었습니다.\n해당 정보로 로그인이 가능합니다.", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(LoginActivity.this, "\t\t\t\t가입이 신청되었습니다.\n관리자 승인 후 로그인이 가능합니다.", Toast.LENGTH_SHORT).show();
 
                             initMyAPI();
 
