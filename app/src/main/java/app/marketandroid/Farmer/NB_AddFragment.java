bab078caa3612b8812bf339bb65ee4816185c0d9
LeeJaeYoung <tekken5953@naver.com>
2020-08-13 13:55:54 +0900

    WIP

--- app/src/main/java/app/marketandroid/Farmer/NB_AddFragment.java
+++ app/src/main/java/app/marketandroid/Farmer/NB_AddFragment.java
@@ -5,29 +5,30 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.app.AlertDialog;
-import android.graphics.drawable.Drawable;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Objects;
 
 import app.marketandroid.R;
 import app.marketandroid.Retrofit.MyAPI;
 import app.marketandroid.Retrofit.PostItem;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -35,12 +36,14 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class NB_AddFragment extends Fragment {
 
     ViewGroup viewGroup;
-    Button btn_potato, btn_apple, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12;
     TextView main_title;
-    Drawable alpha_potato, alpha_apple;
-    ArrayList<String> list = new ArrayList<>();
-    ArrayAdapter<String> adapter;
+    ArrayList<String> list_count = new ArrayList<>();
+    ArrayList<String> list_weight = new ArrayList<>();
+    ArrayAdapter<String> adapter_count;
+    ArrayAdapter<String> adapter_weight;
     MyAPI mMyAPI;
+    int text_position = 0;
+    ArrayList<String> text = new ArrayList<>();
     final PostItem item = new PostItem();
 
     @Override
@@ -50,40 +53,56 @@ public class NB_AddFragment extends Fragment {
 
         main_title = getActivity().findViewById(R.id.main_title);
         main_title.setText("출하물품 등록");
-        btn_apple = getActivity().findViewById(R.id.button2);
-        btn_potato = getActivity().findViewById(R.id.button);
-        btn3 = getActivity().findViewById(R.id.button3);
-        btn4 = getActivity().findViewById(R.id.button4);
-        btn5 = getActivity().findViewById(R.id.button5);
-        btn6 = getActivity().findViewById(R.id.button6);
-        btn7 = getActivity().findViewById(R.id.button7);
-        btn8 = getActivity().findViewById(R.id.button8);
-        btn9 = getActivity().findViewById(R.id.button9);
-        btn10 = getActivity().findViewById(R.id.button10);
-        btn11 = getActivity().findViewById(R.id.button11);
-        btn12 = getActivity().findViewById(R.id.button12);
-        for (int i = 1; i <= 4; i++) {
-            list.add(String.valueOf(10 * i));
+
+        list_weight.add(0,"눌러서 선택");
+        list_count.add(0,"눌러서 선택");
+        for (int i = 1; i <= 9; i++) {
+            list_count.add(String.valueOf(i));
         }
-        adapter = new ArrayAdapter<>(getContext(), R.layout.spinneritem, list);
 
-        buttonClick(btn_potato);
-        buttonClick(btn_apple);
-        buttonClick(btn3);
-        buttonClick(btn4);
-        buttonClick(btn5);
-        buttonClick(btn6);
-        buttonClick(btn7);
-        buttonClick(btn8);
-        buttonClick(btn9);
-        buttonClick(btn10);
-        buttonClick(btn11);
-        buttonClick(btn12);
+        for (int i = 1; i <= 5; i++){
+            list_weight.add(String.valueOf(10 * i));
+        }
+        adapter_count = new ArrayAdapter<>(getContext(), R.layout.spinneritem, list_count);
+        adapter_weight = new ArrayAdapter<>(getContext(), R.layout.spinneritem, list_weight);
 
-        alpha_potato = btn_potato.getBackground();
-        alpha_potato.setAlpha(100);
-        alpha_apple = btn_apple.getBackground();
-        alpha_apple.setAlpha(100);
+        int img[] = {
+                R.drawable.recycle_potato,
+                R.drawable.recycle_apple,
+                R.drawable.recycle_potato,
+                R.drawable.recycle_apple,
+                R.drawable.recycle_potato,
+                R.drawable.recycle_apple,
+                R.drawable.recycle_potato,
+        };
+
+        for (int i = 1; i<= 7; i++){
+            if( i % 2 == 1){
+                text.add("감자");
+            }else{
+                text.add("사과");
+            }
+        }
+
+        MyAdapter adapter = new MyAdapter(
+                getActivity().getApplicationContext(),
+                R.layout.gridview_row,       // GridView 항목의 레이아웃 row.xml
+                img,
+                text);    // 데이터
+
+        GridView gv = (GridView) getActivity().findViewById(R.id.gridView1);
+        gv.setAdapter(adapter);  // 커스텀 아답타를 GridView 에 적용
+
+        // GridView 아이템을 클릭하면 상단 텍스트뷰에 position 출력
+        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                text_position = position;
+                alertDialog();
+            }
+        });
+
     }
 
     @Nullable
@@ -96,9 +115,7 @@ public class NB_AddFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        alpha_potato.setAlpha(255);
-        alpha_apple.setAlpha(255);
-        adapter.clear();
+        adapter_count.clear();
     }
 
     public void alertDialog() {
@@ -109,55 +126,42 @@ public class NB_AddFragment extends Fragment {
         Objects.requireNonNull(alertDialog.getWindow()).getAttributes().windowAnimations = R.style.PauseDialogAnimation;
         final Button add_btn = view.findViewById(R.id.add_dial_add_btn);
         final Button cancel_btn = view.findViewById(R.id.add_dial_cancel_btn);
-        final Spinner spinner = view.findViewById(R.id.add_dial_sell_spinner);
-        spinner.setAdapter(adapter);
-        spinner.setMinimumHeight(110);
-        spinner.setDropDownVerticalOffset(spinner.getMinimumHeight());
+        final Spinner spinner_count = view.findViewById(R.id.add_dial_sell_spinner);
+        final TextView add_dial_title = view.findViewById(R.id.add_dial_title);
+        final Spinner spinner_weight = view.findViewById(R.id.add_dial_weight_spinner);
+        final RelativeLayout relativeLayout = view.findViewById(R.id.add_dial_layout);
+        add_dial_title.setText(text.get(text_position) +" 판매 신청");
+
+        spinner_weight.setAdapter(adapter_weight);
+        spinner_weight.setMinimumHeight(110);
+        spinner_weight.setSelection(0);
+        spinner_weight.setDropDownVerticalOffset(spinner_weight.getMinimumHeight());
+        spinner_weight.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (spinner_weight.getSelectedItemPosition() != 0){
+                    relativeLayout.setVisibility(View.VISIBLE);
+                }else{
+                    relativeLayout.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                relativeLayout.setVisibility(View.GONE);
+
+            }
+        });
+
+        spinner_count.setAdapter(adapter_count);
+        spinner_count.setMinimumHeight(110);
+        spinner_count.setSelection(0);
+        spinner_count.setDropDownVerticalOffset(spinner_count.getMinimumHeight());
+
         alertDialog.setCanceledOnTouchOutside(false);
         add_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Call<PostItem> get_call = mMyAPI.get_products();
-//                get_call.enqueue(new Callback<PostItem>() {
-//                    @Override
-//                    public void onResponse(Call<PostItem> call, Response<PostItem> response) {
-//                        if (response.isSuccessful()){
-//                            PostItem item = new PostItem();
-//                            Log.d("retrofit","valid");
-////                            Log.d("retrofit", item.getProducts_id()+"\n"+item.get);
-//                        }else{
-//                            Log.d("retrofit","invalid");
-//                            Log.d("retrofit",String.valueOf(response.code()));
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Call<PostItem> call, Throwable t) {
-//
-//                    }
-//                });
-
-
-//                Call<PostItem> post_call = mMyAPI.post_products(4,item);
-//                item.setWeight("20");
-//                item.setProducts_id("4");
-//                post_call.enqueue(new Callback<PostItem>() {
-//                    @Override
-//                    public void onResponse(Call<PostItem> call, Response<PostItem> response) {
-//                        if (response.isSuccessful()) {
-//                            Log.d("retrofit", "등록 완료");
-//                        } else {
-//                            Log.d("retrofit", "Status Code : " + response.code());
-//                            Log.d("retrofit", response.errorBody().toString());
-//                            Log.d("retrofit", call.request().body().toString());
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Call<PostItem> call, Throwable t) {
-//                        Log.d("retrofit", "Fail msg : " + t.getMessage());
-//                    }
-//                });
                 Toast.makeText(getContext(), "등록 완료", Toast.LENGTH_SHORT).show();
                 alertDialog.dismiss();
             }
@@ -171,14 +175,47 @@ public class NB_AddFragment extends Fragment {
         alertDialog.show();
     }
 
-    public void buttonClick(final Button btn) {
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                item.setProducts_id(btn.getText().toString());
-                alertDialog();
-            }
-        });
+    static class MyAdapter extends BaseAdapter {
+        Context context;
+        int layout;
+        int[] img;
+        ArrayList<String> text;
+        LayoutInflater inf;
+
+        public MyAdapter(Context context, int layout, int[] img, ArrayList<String> text) {
+            this.context = context;
+            this.layout = layout;
+            this.img = img;
+            this.text = text;
+            inf = (LayoutInflater) context.getSystemService
+                    (Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return img.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return text.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null)
+                convertView = inf.inflate(layout, null);
+            ImageView iv = (ImageView) convertView.findViewById(R.id.imageView1);
+            TextView tv = (TextView) convertView.findViewById(R.id.textView1);
+            iv.setImageResource(img[position]);
+            tv.setText(text.get(position));
+            return convertView;
+        }
     }
 
     private void initMyAPI() {
